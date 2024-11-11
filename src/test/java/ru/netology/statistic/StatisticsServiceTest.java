3fa4bc343d7a34101b2e6bdc21cc245f1a21bcfe
DilyaShakirova <shakirovadilya@yandex.ru>
2024-11-11 22:08:26 +0300

    add JaCoCo

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,13 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxNotFirstIncome(){
+        StatisticsService service=new StatisticsService();
+        long[] incomeInBillions={5,8,4,5,12,3,8,6,11,11,12};
+        long expected=12;
+        long actual= service.findMax(incomeInBillions);
+        Assertions.assertEquals(expected,actual);
+    }
 }
